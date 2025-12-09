804502481d50ef85d0722abd01463bff59659ee1
UlisesNiSchreiner <ulises.schreiner.dev@gmail.com>
2025-12-08 23:28:22 -0300

    feat: add init set - 06

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,19 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globals: true,
-    environment: "node",
+    include: ["test/**/*.test.ts"],
     coverage: {
-      reporter: ["text", "lcov"]
+      provider: "v8",
+      reporter: ["text", "json", "html", "lcov"],
+      all: true,
+      include: ["src/**/*.ts"],
+      thresholds: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80,
+      },
+      exclude: ["src/index.ts"],
     },
-    include: ["test/**/*.test.ts"]
-  }
+  },
 });
